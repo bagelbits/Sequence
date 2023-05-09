b5bc1abccf7c1728344c677cd5031443c8d85459
Christopher Ward <cdurianward@gmail.com>
2023-05-09 16:24:14 -0700

    Split this up

--- ruby/word_finder.rb
+++ ruby/word_finder.rb
@@ -1,12 +1,12 @@
 # Let's assume this is our list of words
 # WORDS = []
-WORDS = ["good", "god", "dog", "goo", "do", "go"]
+WORDS = %w[good god dog goo do go].freeze
 
 def word_finder(letters)
   words = []
   letters = letters.split('')
-  for i in 0..letters.length-1
-    letters.permutation(i+1).to_a.uniq.each do |perm|
+  (0..(letters.length - 1)).each do |i|
+    letters.permutation(i + 1).to_a.uniq.each do |perm|
       words << perm.join if WORDS.include?(perm.join)
     end
   end
@@ -15,3 +15,5 @@ end
 
 test = 'oogd'
 puts word_finder(test).inspect
+
+letters = 'oogd'.split('')
